f558baf3ad3278a8d1763559961f676b84f5b0f5
Ariel Guerrero <ariel.guerrero@ucuenca.edu.ec>
2023-11-13 00:22:21 -0500

    new

---
+++ sketch.js
@@ -0,0 +1,13 @@
+// Modelaremos una particula, luego un conjunto de partículas, utilizando el concepto de clases
+// CLASES: Nos permitirá modelar un objeto o algún modelado de objeto, que luego podremos duplicar en instancias de esta clase
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  // circle(mouseX, mouseY, 20);
+  // - Programaremos una partícula, un elemento grafico quien va a tener un punesto X y una posicion Y.
+  // Vamos a agregar una velocidad a esta particula, determinando una posicon atraves de un vector que determinaramos con variables de vx y vy (propiedades de nuestro objeto de particulas)
+  // Representan la velocidad de movimiento horizontal y vertical, que se expresan inicialmente en pixeles por cuadrado ejemplo: si vx=2 significa que cada fotograma el circula se desplazara 2 pixeles
+}
